f470d03762f375fc550a7c5ca2986f2769fb5f83
105178766 <105178766@student.swin.edu.au>
2025-05-06 22:17:21 +1000

    fixed submission form error in search_form.php

--- search_form.php
+++ search_form.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
   <title>Car Search</title>
 </head>
 <body>
-  <form method="GET" action="search_result.php">
+  <form method="GET" action="cars.php"> <!-- fixed submission to cars.php -->
     <label>Search Car Model:</label>
     <input type="text" name="model" required>
     <input type="submit" value="Search">
